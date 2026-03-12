726664d74f18eddfa32bc2119505d2b51960a757
Jackie <jmh37b@mail.umsl.edu>
2026-03-12 11:56:59 -0500

    starting with psedocode

---
+++ worker.cpp
@@ -0,0 +1,8 @@
+# oss execs child
+# worker takes in -t (seconds and nanoseconds)
+# worker attaches to shared memory
+# start loop
+	# check clock
+	# termination = (current time + (-t input))
+# end loop
+# output message
