b7ee42c1c7ac8b11fd576ed16b45fc8fda2d8a21
Jackie <jmh37b@mail.umsl.edu>
2026-03-17 18:36:11 -0500

    started worker.cpp code stucture

--- worker.cpp
+++ worker.cpp
@@ -1,8 +1,52 @@
-# oss execs child
-# worker takes in -t (seconds and nanoseconds)
-# worker attaches to shared memory
-# start loop
-	# check clock
-	# termination = (current time + (-t input))
-# end loop
-# output message
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <iostream>
+
+typedef struct msgbuffer {
+	long mtype;
+	char strData[100];
+	int intData;
+} msgbuffer;
+
+int main() {
+	key_t key;
+	int msqid;
+
+	// get same key as oss
+	key = ftok(".", 'B');
+	if (key == -1) {
+		perror("Worker ftok");
+		exit(1);
+	}
+	
+	msqid = msgget(key, 0644);
+	if (msqid == -1) {
+		perror("worker msgget");
+		exit(1);
+	}
+
+	msgbuffer msg;
+
+	// receive message from oss
+	if (msgrcv(msqid, &msg, sizeof(msg), 0, 0) == -1) {
+		perror("worker msgrcv");
+		exit(1);
+	}
+
+	std::cout << "Worker %d received: %s\n", getpid(), msg.strData;
+
+	// modify message and send back
+	msg.mtype = 1;
+	strcpy(msg.strData, "Reply from worker");
+
+	if (msgsnd(msqid, &msg, sizeof(msg), 0) == -1) {
+		perror("worker msgsnd");
+		exit(1);
+	}
+
+	return 0;
+}
