019d2d5169ea08cfbde35670ae46e901cdcbb51e
Jackie <jmh37b@mail.umsl.edu>
2026-03-17 22:22:29 -0500

    changes

--- worker.cpp
+++ worker.cpp
@@ -12,40 +12,50 @@ typedef struct msgbuffer {
 	int intData;
 } msgbuffer;
 
-int main() {
+int main(int argc, char *argv[]) {
 	key_t key;
 	int msqid;
 
-	// get same key as oss
-	key = ftok(".", 'B');
-	if (key == -1) {
-		perror("Worker ftok");
-		exit(1);
-	}
-	
+	// get message queue
+	key = ftok(".", 'B');	
 	msqid = msgget(key, 0644);
-	if (msqid == -1) {
-		perror("worker msgget");
-		exit(1);
-	}
 
 	msgbuffer msg;
+	
+	int messagesReceived = 0;
+	
+	// FAKE termination condition
+	int maxLoops = (rand() % 5) + 3;
 
-	// receive message from oss
-	if (msgrcv(msqid, &msg, sizeof(msg), 0, 0) == -1) {
-		perror("worker msgrcv");
-		exit(1);
-	}
+	std::cout << "Worker PID:%d STARTING\n", getpid();
+	
+	while (1) {
+		// wait for oss
+		if (msgrcv(msqid, &msg, sizeof(msg), getpid(), 0) == -1) {
+			perror("worker msgrcv");
+			exit(1);
+		}
 
-	std::cout << "Worker %d received: %s\n", getpid(), msg.strData;
+		messagesReceived++;
 
-	// modify message and send back
-	msg.mtype = 1;
-	strcpy(msg.strData, "Reply from worker");
+		std::cout << "WORKER PID:%d received message  %d\n", getpid(), messagesReceived;
 
-	if (msgsnd(msqid, &msg, sizeof(msg), 0) == -1) {
-		perror("worker msgsnd");
-		exit(1);
+		// decide if terminating
+		if (messagesReceived >= maxLoops) {
+			msg.intData = 0;
+			std::cout << "WORKER PID:%d TERMINATING\n", getpid();
+		} else {
+			msg.intData = 1;
+		}
+
+		msg.mtype = 1;
+
+		if (msgsnd(msqid, &msg, sizeof(msg), 0) == -1) {
+			perror("worker msgsnd");
+			exit(1);
+		}
+
+		if (msg.intData == 0) break;
 	}
 
 	return 0;
